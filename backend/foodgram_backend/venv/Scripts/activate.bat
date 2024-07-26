7374d15e643c91efd9bcc3e02e110604bc73dc8e
Александр <sasatoguzov@gmail.com>
2024-07-26 11:43:33 +0300

    CODE

--- backend/foodgram_backend/venv/Scripts/activate.bat
+++ backend/foodgram_backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Dev\foodgram\venv
+set VIRTUAL_ENV=D:\Dev\foodgram\backend\foodgram_backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
